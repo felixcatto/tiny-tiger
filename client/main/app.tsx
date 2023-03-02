e111f5efa0bd8f1eeba6e4a55671edfc2e42ce34
felix <felix0808@yandex.ru>
2023-03-02 15:21:42 +0400

    continue setup css..

--- client/main/app.tsx
+++ client/main/app.tsx
@@ -3,9 +3,11 @@ import s from './styles.module.css';
 import cn from 'classnames';
 
 const App = () => {
-  return <div className={s.white}>
-    <div className="container mx-auto">Hello world!</div>
-  </div>;
+  return (
+    <div className={s.white}>
+      <div className="container">Hello world!</div>
+    </div>
+  );
 };
 
 export default App;
